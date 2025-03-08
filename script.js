4fe688f0d9e001a7e7b63ba80e42aae7557b4d07
Andres Rodriguez <anferoro2004@gmail.com>
2025-03-08 12:02:16 -0500

    Ventanas modal cards

--- script.js
+++ script.js
@@ -141,6 +141,64 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
     
-    // Inicializar el menú interactivo
+
     menuInteractivo.init();
+
+    const windowBackgroud = document.getElementById("window-backgroud"),
+    windowContainer = document.getElementById("window-container"),
+    closeButton = document.getElementById("close-button")
+
+
+
+
+const modal = document.getElementById("window-backgroud");
+const modalTitle = modal.querySelector(".modal-title");
+const modalImage = modal.querySelector(".imagen-modal");
+const modalDescription = modal.querySelector(".modal-description");
+
+
+const productos = {
+    "Baklava": {
+        title: "Baklava",
+        img: "public/imagenes_tarjetas/imagen_backla.png",
+        description: "Baklava es un postre tradicional de la cocina del Medio Oriente, los Balcanes y el Mediterráneo, elaborado con capas finas de masa filo rellenas de frutos secos, como nueces, almendras o pistachos, y endulzado con almíbar o miel."
+    },
+    "Menemen": {
+        title: "Menemen",
+        img: "public/imagenes_tarjetas/modal_menemen.png",
+        description: "El Menemen es un desayuno tradicional turco preparado con huevos revueltos, pimientos, tomates y especias. Se sirve caliente y se disfruta con pan fresco."
+    },
+    "Börek": {
+        title: "Börek",
+        img: "public/imagenes_tarjetas/borek_modal.png",
+        description: "Börek es un pastel salado turco hecho con masa filo y relleno de queso, carne o verduras. Es un plato muy popular en la cocina turca y de los Balcanes."
+    },
+    "Dolma": {
+        title: "Dolma",
+        img: "public/imagenes_tarjetas/modal_dolma.png",
+        description: "El Dolma es un plato relleno de arroz, carne y especias, envuelto en hojas de parra o verduras. Es muy popular en la gastronomía de Medio Oriente y el Mediterráneo."
+    }
+};
+
+
+document.querySelectorAll(".btn_add").forEach(button => {
+    button.addEventListener("click", function () {
+
+        const card = this.closest(".tarjetas__container");
+        const title = card.querySelector("h2").innerText;
+
+
+        if (productos[title]) {
+            modalTitle.innerText = productos[title].title;
+            modalImage.src = productos[title].img;
+            modalDescription.innerText = productos[title].description;
+            modal.style.display = "flex";
+        }
+    });
+});
+
+closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
 });
